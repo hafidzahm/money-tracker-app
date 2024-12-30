cca9e337e7526fd654a791c66965ce2810138ba4
Abdul <hafizhahm123@gmail.com>
2024-12-30 10:52:40 +0700

    bugfix typo

--- webpack.common.js
+++ webpack.common.js
@@ -1,51 +1,49 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const loader = require('sass-loader');
-const { postcss } = require('autoprefixer');
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+ 
 module.exports = {
-    entry: {
-        app: path.resolve(__dirname, 'src/js/index.js')
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module:{
-        rules: [
-            {
-                test: /\.(s[ac]ss)$/i,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: () => [require('autoprefixer')],
-                            },
-                        },
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ],
+  entry: {
+    app: path.resolve(__dirname, 'src/js/index.js'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]ss)$/i,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [require('autoprefixer')],
+              },
             },
+          },
+          {
+            loader: 'sass-loader',
+          },
         ],
-    },
-    pllugins: [
-        new HtmlWebpackPlugin({
-            title: 'Dashboard',
-            filename: 'index.html',
-            template: path.resolve(__dirname, 'src/views/index.html')
-        }),
-
-        new CleanWebpackPlugin()
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Dashboard',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/views/index.html'),
+    }),
+ 
+    new CleanWebpackPlugin(),
+  ],
 };
